3d9ec7d8261cdbc42bb77a9c90e0eb00fe6258a1
Francisco Muniz <fmuniz@encore.am>
2024-02-01 13:28:03 -0300

    fix entrypoint command, removing extra -

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,4 +3,4 @@
 # This script is used to set the environment variables and start the application
 
 # Use environment variables directly in the entrypoint script
-exec /external-dns-porkbun-webhook --log-level=debug - --domain-filter=${PORKBUN_DOMAIN_FILTER} --porkbun-api-secret=${PORKBUN_API_SECRET} --porkbun-api-key=${PORKBUN_API_KEY}
+exec /external-dns-porkbun-webhook --log-level=debug --domain-filter=${PORKBUN_DOMAIN_FILTER} --porkbun-api-secret=${PORKBUN_API_SECRET} --porkbun-api-key=${PORKBUN_API_KEY}
